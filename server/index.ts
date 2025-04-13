21c9f34a50c9b5a801c681baa7e1ba02b36242cb
raoufboukh <zakib2316@gmail.com>
2025-04-13 10:35:13 +0100

    added some configuration at index.ts

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,11 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
+import router from "./routers/auth.routers.ts";
+import MongoStore from "connect-mongo";
+import session from "express-session";
+import configurePassport from "./controllers/auth.controllers.ts";
+import passport from "passport";
 
 const app = express();
 dotenv.config();
@@ -11,13 +16,28 @@ const connectionUrl = process.env.CONNECTION_URL || "";
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", ""],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "library_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: connectionUrl,
+    }),
+  })
+);
+
+configurePassport(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use("/auth", router);
 
 mongoose.connect(connectionUrl).then(() => {
   console.log("Connected to MongoDB");
