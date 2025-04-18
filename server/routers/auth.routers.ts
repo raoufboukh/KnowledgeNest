46049b42d6368904ec0cb5b4308f25c338b04dce
raoufboukh <zakib2316@gmail.com>
2025-04-18 20:24:42 +0100

    modify routers

--- server/routers/auth.routers.ts
+++ server/routers/auth.routers.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import passport from "passport";
 import {
+  checkAuth,
   failed,
   logout,
   register,
@@ -8,7 +9,7 @@ import {
 } from "../controllers/auth.controllers.ts";
 
 const router = express.Router();
-
+router.get("/check", checkAuth);
 router.post("/register", register);
 router.post("/login", passport.authenticate("local"), (req, res) => {
   res.status(200).json({ message: "Login successful", user: req.user });
@@ -21,9 +22,14 @@ router.get(
   "/google/callback",
   passport.authenticate("google", {
     failureRedirect: "/auth/failed",
-    successRedirect: "/auth/success",
-  })
+    session: true,
+  }),
+  (req, res) => {
+    console.log("Google Auth Success:", req.user);
+    res.redirect("http://localhost:3000/");
+  }
 );
+
 router.get("/success", success);
 router.get("/failed", failed);
 router.get("/logout", logout);
