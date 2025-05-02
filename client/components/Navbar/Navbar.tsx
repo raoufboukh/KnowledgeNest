df835f8143039c3c44e1e4a700392134e93309de
raoufboukh <zakib2316@gmail.com>
2025-05-02 17:56:09 +0100

    create navbar component

---
+++ client/components/Navbar/Navbar.tsx
@@ -0,0 +1,62 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { links } from "../constants";
+import { RiMenu3Fill, RiCloseFill } from "react-icons/ri";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  return (
+    <header className="fixed top-0 left-0 z-50 w-full  bg-black/70">
+      <div className="container mx-auto p-4 flex justify-between border-b border-gray-700 items-center ">
+        <Link href="/" className="text-3xl font-bold">
+          Cars
+        </Link>
+        <RiMenu3Fill
+          className="lg:hidden cursor-pointer"
+          onClick={() => setIsOpen(!isOpen)}
+        />
+        <nav
+          className={
+            "flex lg:flex-row flex-col gap-6 text-lg font-semibold  lg:justify-between lg:px-0 px-4 lg:items-center lg:top-0 lg:left-0 bg-black lg:h-fit h-screen top-0 right-0 transition-all duration-300 lg:relative absolute w-50 lg:w-[60%]" +
+            (isOpen ? " translate-x-0" : " translate-x-full lg:translate-x-0")
+          }
+        >
+          <RiCloseFill
+            className="lg:hidden cursor-pointer right-5 text-2xl absolute top-4"
+            onClick={() => {
+              setIsOpen(!isOpen);
+              console.log(isOpen);
+            }}
+          />
+          <div className="flex flex-col lg:flex-row lg:py-0 pt-20 gap-4 lg:gap-20">
+            {links.map(
+              (link, i) =>
+                link.title !== "Sign" && (
+                  <Link key={i} href={link.link}>
+                    {link.title}
+                  </Link>
+                )
+            )}
+          </div>
+          <div>
+            {links.map(
+              (link, i) =>
+                link.title === "Sign" && (
+                  <Link
+                    key={i}
+                    href={link.link}
+                    className="bg-primary px-4 py-2 rounded-md text-white"
+                  >
+                    {link.title}
+                  </Link>
+                )
+            )}
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ client/components/constants.ts
@@ -0,0 +1,18 @@
+export const links = [
+  {
+    title: "Buy",
+    link: "/buy",
+  },
+  {
+    title: "Sell",
+    link: "/sell",
+  },
+  {
+    title: "About",
+    link: "/about",
+  },
+  {
+    title: "Sign",
+    link: "/login",
+  },
+];
