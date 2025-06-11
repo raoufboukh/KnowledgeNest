50fd10c366b7f7551928537b070b5b2f025a1541
raoufboukh <zakib2316@gmail.com>
2025-06-11 15:43:47 +0100

    Update global styles and enhance homepage layout with new components; add image assets

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,19 @@
+import About from "@/components/about/About";
+import Brands from "@/components/cars/Brands";
+import Cars from "@/components/cars/Cars";
+import Footer from "@/components/footer/Footer";
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar/Navbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Brands />
+      <Cars />
+      <About />
+      <Footer />
     </div>
   );
 }
